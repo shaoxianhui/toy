7de28e754db1d88e2f45f880963a681d0b4da123
shaoxianhui <huihui6007@163.com>
2015-01-04 23:16:57 +0800

    上一次提交遗失的文件

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var fullpage = require('./routes/fullpage');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/fullpage', fullpage);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
